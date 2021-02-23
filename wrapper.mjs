10d0447042870e23f79a34cbcc53cb5fe8b5a94c
Melissa McEwen <melissa@skypack.dev>
2021-02-23 13:37:28 -0600

    adding named exports

---
+++ wrapper.mjs
@@ -0,0 +1,6 @@
+import spookyWords from './index.cjs';
+
+export const objects = spookyWords.objects;
+export const predicates = spookyWords.predicates;
+export const teams = spookyWords.teams;
+export const collections = spookyWords.collections;
